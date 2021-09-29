1e4d1e9695777fc80bbaf39b90c2d215fa7a1981
Maximiles Barcelos <61763153+MaXximiles@users.noreply.github.com>
2021-09-29 20:22:03 -0300

    Atualizando para conexão com Heroku

--- src/main/java/fatec/grupodois/endurance/EnduranceApplication.java
+++ src/main/java/fatec/grupodois/endurance/EnduranceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 import java.io.File;
+import java.util.Arrays;
 import java.util.Collections;
 
 import static fatec.grupodois.endurance.constant.FileConstant.USER_FOLDER;
@@ -27,7 +28,7 @@ public class EnduranceApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+		corsConfiguration.setAllowedOrigins(Arrays.asList(new String[]{"http://localhost:4200", "https://enduranceweb.herokuapp.com"}));
 		corsConfiguration.setAllowedHeaders(asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
