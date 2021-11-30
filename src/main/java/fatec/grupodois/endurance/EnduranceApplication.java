0e7f1b7c882279062d15da17b6c63dd87c07a86e
Jeferson Neves <62185462+jef771@users.noreply.github.com>
2021-11-30 12:28:29 -0300

    Update EnduranceApplication.java

--- src/main/java/fatec/grupodois/endurance/EnduranceApplication.java
+++ src/main/java/fatec/grupodois/endurance/EnduranceApplication.java
@@ -26,7 +26,7 @@ public class EnduranceApplication {
 		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(asList("http://localhost:4200", "https://enduranceweb.herokuapp.com"));
+		corsConfiguration.setAllowedOrigins(asList("http://localhost:4200", "https://enduranceweb.herokuapp.com", "https://endurance-app.netlify.app"));
 		corsConfiguration.setAllowedHeaders(asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
 				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
